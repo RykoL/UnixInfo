26bda252a43afd9176180076ce0549858034dd98
RykoL <rico.lang@vr-web.de>
2015-04-29 14:08:23 +0200

    added a few tests in the main file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include "include/Information.h"
 #include "include/DirectoryInfo.h"
+#include "include/Path.h"
+#include "include/ProcessInformation.h"
 
 int main(int argc, char* argv[])
 {
@@ -10,8 +12,17 @@ int main(int argc, char* argv[])
 	std::cout<< info.getExecutionPath() << std::endl;
 	std::cout<< info.getLanguage() << std::endl;
 
-	System::IO::DirectoryInfo dInfo("/home/rico");
-	std::cout<< dInfo.getLastAccessTime() << "\n" << dInfo.getLastModifiedTime() << "\n" << dInfo.getStatusChangedTime() << std::endl;;
+	System::IO::DirectoryInfo dInfo("/home/rico/Programmieren");
+	//dInfo.create("test", static_cast<System::IO::Permission>(System::IO::Permission::User_Read | System::IO::Permission::Group_Read /*| System::IO::Permission::User_Write*/));
+	//dInfo.deleteDir();
+	std::cout<< dInfo.getLastAccessTime() << "\n" << dInfo.getLastModifiedTime() << "\n" << dInfo.getStatusChangedTime() << std::endl;
+	/*
+	unsigned int pid;
+	std::cin >> pid;
 
+	System::ProcessInformation pInfo{pid};
+	*/
+
+	System::ProcessInformation::start("/home/rico/Programmieren/C++/system/tests/args", {"hudelblub","turen frass", "Bohse", "Paulaner"});
 	return 0;
 }
