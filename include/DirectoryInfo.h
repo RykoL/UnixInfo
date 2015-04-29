7b9915a2a53a952b2732b047dc89b8b24202ddf6
RykoL <rico.lang@vr-web.de>
2015-04-29 13:59:02 +0200

    Added Permission Enum and added a few functions

--- include/DirectoryInfo.h
+++ include/DirectoryInfo.h
@@ -10,26 +10,50 @@
 #include <stdexcept>
 #include <memory>
 #include <ctime>
+#include <cstring>
 #include <sstream>
 #include <string>
+#include <iostream>
 
 namespace System
 {
 
 namespace IO
 {
+	enum Permission{
+		User_Read = S_IRUSR,
+		User_Write = S_IWUSR,
+		User_Execute = S_IXUSR,
+		Group_Read = S_IRGRP,
+		Group_Write = S_IWGRP,
+		Group_Execute = S_IXGRP,
+		Other_Read = S_IROTH,
+		Other_Write = S_IWOTH,
+		Other_Execute = S_IXOTH
+	};
+
 	class DirectoryInfo
 	{
 
 	public:
 		DirectoryInfo(const std::string &path);
+		DirectoryInfo() {};
 		~DirectoryInfo();
 
 		bool open(const std::string &path);
+		std::unique_ptr<DirectoryInfo> create(const std::string &path, Permission pm);
+		void deleteDir();
+		void deleteDir(const std::string &path);
+		void close();
+
 		std::vector<std::unique_ptr<DirectoryInfo>> getDirectories();
+		std::vector<std::string> getFileNames();
+		std::string getFullPath() {return mPath;};
 		std::string getLastAccessTime() {return ConvertToTimeString(mAccessTime);};
 		std::string getLastModifiedTime() {return ConvertToTimeString(mModificationTime);};
 		std::string getStatusChangedTime() {return ConvertToTimeString(mStatusChangeTime);};
+
+		static bool Exists(const std::string &path);
 
 	private:
 		void getDirectoryInfo();
@@ -43,7 +67,7 @@ namespace IO
 		time_t mAccessTime;
 		time_t mModificationTime;
 		time_t mStatusChangeTime;
-
+		mode_t mPermission;
 	};
 }
 }
--- src/DirectoryInfo.cpp
+++ src/DirectoryInfo.cpp
@@ -16,10 +16,15 @@ namespace IO
 		
 		if((mFileDescriptor = dirfd(mDirectoryPointer)) == -1)
 		{
-			throw std::runtime_error("can't obtain file descriptor, failed with error code: " + std::to_string(errno));
+			throw std::runtime_error("can't obtain file descriptor, failed with error code: " + std::string{std::strerror(errno)});
 		}
 
 		getDirectoryInfo();
+	}
+
+	DirectoryInfo::~DirectoryInfo()
+	{
+		closedir(mDirectoryPointer);
 	}
 
 	void DirectoryInfo::getDirectoryInfo()
@@ -28,18 +33,14 @@ namespace IO
 
 		if(fstat(mFileDescriptor, &dStat) != 0)
 		{
-			throw std::runtime_error("DirectoryInfo::getDirectoryInfo failed with error code: " + std::to_string(errno));
+			throw std::runtime_error("DirectoryInfo::getDirectoryInfo failed with error code: " + std::string{std::strerror(errno)});
 		}
 
 		mAccessTime = dStat.st_atime;
 		mModificationTime = dStat.st_mtime;
 		mStatusChangeTime = dStat.st_ctime;
-	}
-
-	DirectoryInfo::~DirectoryInfo()
-	{
-		closedir(mDirectoryPointer);
-	}
+		mPermission = dStat.st_mode;
+	}	
 
 	bool DirectoryInfo::open(const std::string &path)
 	{
@@ -49,6 +50,62 @@ namespace IO
 		}
 
 		return true;
+	}
+
+	void DirectoryInfo::close()
+	{
+		mPath = "";
+		mFileDescriptor = -1;
+		mAccessTime = 0;
+		mModificationTime = 0;
+		mStatusChangeTime = 0;
+
+		closedir(mDirectoryPointer);
+	}
+
+	std::unique_ptr<DirectoryInfo> DirectoryInfo::create(const std::string &path,Permission pm)
+	{
+		umask(0);
+
+		if (mkdir(path.c_str(), pm) == -1)
+		{
+			throw std::runtime_error("DirectoryInfo::createDirectory failed with error code: " + std::string{std::strerror(errno)});
+		}
+
+		return std::unique_ptr<DirectoryInfo>(new DirectoryInfo(path));
+	}
+
+	void DirectoryInfo::deleteDir()
+	{
+		if(access(mPath.c_str(), F_OK))
+		{
+			if(!rmdir(mPath.c_str()))
+			{
+				throw std::runtime_error("DirectoryInfo::deleteDirectory failed with error code: " + std::string{std::strerror(errno)});
+			}
+
+
+		}
+		else
+		{
+			std::cerr<< "No acces to the file " << mPath << std::endl;
+		}
+	}
+
+	void DirectoryInfo::deleteDir(const std::string &path)
+	{
+		if(access(path.c_str(), F_OK))
+		{
+			if(!rmdir(path.c_str()))
+			{
+				throw std::runtime_error("DirectoryInfo::deleteDirectory failed with error code: " + std::string{std::strerror(errno)});
+			}
+		}
+
+		else
+		{
+			std::cerr << "No accss to the file " << path << std::endl;
+		}
 	}
 
 	std::vector<std::unique_ptr<DirectoryInfo>> DirectoryInfo::getDirectories()
@@ -68,6 +125,23 @@ namespace IO
 		return dirs;
 	}
 
+	std::vector<std::string> DirectoryInfo::getFileNames()
+	{
+		std::vector<std::string> FileNames;
+
+		dirent *dirp;
+
+		while((dirp = readdir(mDirectoryPointer)) != nullptr)
+		{
+			if(dirp->d_type == DT_REG)
+			{
+				FileNames.push_back(dirp->d_name);
+			}
+		}
+
+		return FileNames;
+	}
+
 	std::string DirectoryInfo::ConvertToTimeString(time_t t)
 	{
 		std::tm *tm = std::localtime(&t);
@@ -76,6 +150,19 @@ namespace IO
 		ss << tm->tm_mday << "-" << tm->tm_mon + 1<< "-" << tm->tm_year + 1900 << " " << tm->tm_hour << ":" << tm->tm_min << ":" << tm->tm_sec;
 
 		return ss.str();
+	}
+
+	bool DirectoryInfo::Exists(const std::string &path)
+	{
+		DIR *dir;
+
+		if((dir = opendir(path.c_str())) != nullptr)
+		{
+			closedir(dir);
+			return true;
+		}
+
+		return false;
 	}
 }
 
