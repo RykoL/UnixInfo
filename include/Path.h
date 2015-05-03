d2461cc9f306bfaa02f2671d3728a3db81f24c2a
RykoL <rico.lang@vr-web.de>
2015-05-03 20:55:55 +0200

    added a helper method

---
+++ include/Path.h
@@ -0,0 +1,39 @@
+#ifndef PATH_H
+#define PATH_H
+
+#include <string>
+#include <iostream>
+
+namespace System
+{
+
+	namespace IO
+	{
+
+		namespace Path
+		{
+			std::string Combine(std::string path1,std::string path2)
+			{
+				if(path1.length() == 0 || path2.length() == 0)
+				{
+					return (path1.length() == 0) ? path2 : path1;
+				}
+
+				if(path1.back() == '/' && path2.front() == '/')
+				{
+					return path1 + path2.erase(0,1);
+				}
+
+				if(path1.back() == '/' && path2.front() != '/')
+				{
+					return path1 + path2;
+				}
+
+				return "";
+			}
+		}
+	}
+
+}
+
+#endif
